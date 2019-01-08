2b43487e61f4b681db95c30526bc5dffa74503a4
Alexander Ivankov <alexanderivankoff@gmail.com>
2019-01-08 22:56:17 +0300

    feat: add first Vue component

---
+++ src/components/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Привет, {{name}}!</div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      name: 'Александр Иванков'
+    }
+  }
+}
+</script>

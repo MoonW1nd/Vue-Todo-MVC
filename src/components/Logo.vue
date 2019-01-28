ff820e6f4248803cffcf13854641f721ce3ae622
Alexander Ivankov <ivankov@uchi.ru>
2019-01-28 20:07:18 +0300

    feat: add Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <img class="Logo" v-on="$listeners" v-bind="$attrs"/>
+</template>
+
+<script>
+/**
+ * @class Logo
+ * @description логотип
+ */
+export default {
+  name: 'Logo',
+  inheritAttrs: false
+}
+</script>
+
+<style lang="scss" scoped>
+.Logo {
+  opacity: 0.8;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,9 @@
 <template>
   <main class="Page Page_main">
+    <Logo
+      :src="src"
+      class="Page-Logo"
+    />
     <TodoInputContainer placeholder="Add task"/>
     <OpenedTodoListContainer/>
   </main>
@@ -7,12 +11,19 @@
 
 <script>
 import OpenedTodoListContainer from '@/containers/OpenedTodoList.container.vue'
+import Logo from '@/components/Logo.vue'
+import LogoImage from '@/assets/Logo_Todo.svg'
 import TodoInputContainer from '@/containers/TodoInput.container.vue'
 
 export default {
   name: 'TodoList',
+  data () {
+    return {
+      src: LogoImage
+    }
+  },
   props: ['todos'],
-  components: { OpenedTodoListContainer, TodoInputContainer }
+  components: { OpenedTodoListContainer, TodoInputContainer, Logo }
 }
 </script>
 
@@ -30,6 +41,20 @@ export default {
     width: calc(50vw - 15px);
     min-width: 350px;
     max-width: 70ch;
+  }
+}
+
+.Page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &-Logo {
+    position: relative;
+    width: calc(20vw - 15px);
+    min-width: 100px;
+    max-width: 190px;
+    margin: calc(10px + (20 - 10) * (100vw - 320px) / (1200 - 320));
   }
 }
 </style>
